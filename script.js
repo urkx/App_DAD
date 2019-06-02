334d5aba6a4341c657fd0417483a2df6e70045ad
Urko Lopez <urkolopezlopez@gmail.com>
2019-06-02 20:11:33 +0200

    primer commit

---
+++ script.js
@@ -0,0 +1,67 @@
+function getData(selector){
+    var estadoTabla = document.getElementById('estadoTabla');
+    var request = new XMLHttpRequest;
+    request.open('GET', 'https://localhost:8090/comidas');
+    request.onload = function () {
+        var data = JSON.parse(this.response);
+    
+        if(request.status >= 200 && request.status < 400){
+            
+                var columns = addAllColumnHeaders(data, selector);
+              
+                for (var i = 0; i < myList.length; i++) {
+                  var row$ = $('<tr/>');
+                  for (var colIndex = 0; colIndex < columns.length; colIndex++) {
+                    var cellValue = myList[i][columns[colIndex]];
+                    if (cellValue == null) cellValue = "";
+                    row$.append($('<td/>').html(cellValue));
+                  }
+                  $(selector).append(row$);
+                }
+             estadoTabla.innerHTML += 'Datos cargados correctamente';
+
+        }else{
+           estadoTabla.innerHTML += 'No se pudieron cargar los datos';
+        }
+    };
+    request.onerror = function(){
+        estadoTabla.innerHTML += 'Fallo de conexión';
+    }
+    request.send();
+}
+
+function addAllColumnHeaders(myList, selector) {
+    var columnSet = [];
+    var headerTr$ = $('<tr/>');
+  
+    for (var i = 0; i < myList.length; i++) {
+      var rowHash = myList[i];
+      for (var key in rowHash) {
+        if ($.inArray(key, columnSet) == -1) {
+          columnSet.push(key);
+          headerTr$.append($('<th/>').html(key));
+        }
+      }
+    }
+    $(selector).append(headerTr$);
+  
+    return columnSet;
+  }
+
+
+function sendForm(){
+    var output = document.getElementById("output");
+    var postRequest = new XMLHttpRequest();
+    var hora = document.getElementById('hora').value;
+    var nombre =document.getElementById('nombre').value;
+    var json = '{"horas": ' + hora + ',' +
+    '"nombreComida": ' + nombre + '}';
+    postRequest.open("POST", 'https://localhost:8090/anyadirComida');
+    postRequest.send(json);
+    postRequest.onerror = function(){alert('fallo');}
+    if(postRequest == 200){
+        output.innerHTML += "Añadida";
+    }else{
+        output.innerHTML += "Error" + postRequest.status + json;
+    }
+}
